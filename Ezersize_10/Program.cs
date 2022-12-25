cde15b68ea7cf5d2242b915459dedbaab8748153
Алёна Катанская <alyona.katanskaya@gmail.com>
2022-12-25 21:28:07 +0300

    add Ezersize 10

--- Ezersize_10/Program.cs
+++ Ezersize_10/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("HW");
+﻿Console.Write("Введите трехзначное число: ");
+while(true)
+{
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
