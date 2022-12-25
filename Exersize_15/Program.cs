175f873f5a5662d861ee5fff988a4f68c78a2dba
Алёна Катанская <alyona.katanskaya@gmail.com>
2022-12-26 01:17:51 +0300

    add Exersize 15

--- Exersize_15/Program.cs
+++ Exersize_15/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите день недели по счету: ");
+string input = Console.ReadLine();
+
+// if(n == 1)
+// {
+//     Console.Write("Понедельник");
+// }
+// else(n == 2);
+// {
+//     Console.Write("Вторник");
+// }
+
+
+//List<string> 
+
+var list = new List<string>()
+{
+    "Понедельник",
+    "Вторник", 
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье"
+};
+
+Console.Write(List[list - 1]);
+
+
